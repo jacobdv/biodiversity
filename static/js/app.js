11e904cfbf1e9445b1e40733c8aaef87b64cc05a
Jacob de Vries <mrjacobdevries@gmail.com>
2021-04-08 14:48:30 -0700

    Populates dropdown list.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,24 @@
-d3.json('samples.json').then(data => {
-    console.log(data);
-})
+d3.json('samples.json').then((sampleData) => {
+
+    // Object of only IDs and values.
+    const allSamples = sampleData.samples;
+
+    // Adds options to dropdown list.
+    let selectionMenu = d3.select('#selDataset');
+    d3.select('#selDataset').on('change', updatePlotly);
+        
+    // Pull top ten OTU values.
+    allSamples.forEach(item => {
+        selectionMenu.append('option')
+            .text(item.id)
+            .attr('value',item.id);
+        item.sample_values.slice(0,10);
+        console.log(item);
+    });
+
+    function updatePlotly() {
+        let dataset = selectionMenu.property('value');
+        console.log(dataset)
+    }
+
+});
