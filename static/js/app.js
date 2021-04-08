108188cd36a87e9024b7d29d66007b617f1c1ba3
Jacob de Vries <mrjacobdevries@gmail.com>
2021-04-08 15:44:45 -0700

    Narrows to selected array, steps in.

--- static/js/app.js
+++ static/js/app.js
@@ -6,19 +6,27 @@ d3.json('samples.json').then((sampleData) => {
     // Adds options to dropdown list.
     let selectionMenu = d3.select('#selDataset');
     d3.select('#selDataset').on('change', updatePlotly);
-        
-    // Pull top ten OTU values.
     allSamples.forEach(item => {
         selectionMenu.append('option')
             .text(item.id)
             .attr('value',item.id);
-        item.sample_values.slice(0,10);
-        console.log(item);
     });
 
+    // Updates graphs to match new ID number.
     function updatePlotly() {
-        let dataset = selectionMenu.property('value');
-        console.log(dataset)
-    }
+        // Grabs selected ID number.
+        let idNumber = selectionMenu.property('value');
+        // Pulls ID number's corresponding data.
+        let selectedSample = allSamples.filter(sample => sample.id === idNumber);
+        // Trims data to ten points.
+        selectedSample[0].otu_ids = selectedSample[0].otu_ids.slice(0,10);
+        selectedSample[0].otu_labels = selectedSample[0].otu_labels.slice(0,10);
+        selectedSample[0].sample_values = selectedSample[0].sample_values.slice(0,10);
+        // Moves into array.
+        selectedSample = selectedSample[0];
+        // Logs selected sample data.
+        console.log(selectedSample);
 
+    }
 });
+  
