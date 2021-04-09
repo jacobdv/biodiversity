a1700e618101efed4b60c2be859ebcb6672e3110
Jacob de Vries <mrjacobdevries@gmail.com>
2021-04-08 17:45:33 -0700

    It works. Should be ready for clean up and submission.

--- static/js/app.js
+++ static/js/app.js
@@ -22,10 +22,10 @@ d3.json('samples.json').then((sampleData) => {
         // Updates demographic information.
         let selectedDemo = (allDemographics.filter(info => String(info.id) === idNumber)[0]);
         let demoPanel = d3.select('#sample-metadata').html('');
+        // Appends h5 with information for each entry in the individual demographics object.
         for (const [key, value] of Object.entries(selectedDemo)) {
             demoPanel.append('h5')
-                .text(`${key}: ${value}`)
-            console.log(key, value)
+                .text(`${key}: ${value}`);
         }
 
         // Pulls ID number's corresponding data.
@@ -43,7 +43,7 @@ d3.json('samples.json').then((sampleData) => {
         labels = selectedSample[0].otu_labels.slice(0,10);
         values = selectedSample[0].sample_values.slice(0,10);
 
-        // Creates trace for individual for plotting.
+        // Creates trace for individual for plotting bar chart.
         let barTrace = {
             y: ids,
             x: values,
@@ -68,6 +68,79 @@ d3.json('samples.json').then((sampleData) => {
         };
         Plotly.newPlot('bar', barData, barLayout);
 
+        // Creates trace for individual for plotting bubble chart.
+        let bubbleTrace = {
+            x: selectedSample[0].otu_ids,
+            y: selectedSample[0].sample_values,
+            text: selectedSample[0].otu_labels,
+            mode: 'markers',
+            marker: { 
+                color: selectedSample[0].otu_ids,
+                size: selectedSample[0].sample_values
+            }
+        };
+        
+        // Plots bubble chart for the sample values in the selected individual.
+        let bubbleData = [bubbleTrace];
+        let bubbleLayout = {
+            title: `OTU ${selectedSample.id} Results`,
+            yaxis: { title: 'Sample Value' },
+            xaxis: { title: 'OTU ID' }
+        };
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+
+
+
+        // Creates trace for individual for plotting gauge.
+        let gaugeTrace = {
+            domain: {
+                x: [0,1],
+                y: [0,1]
+            },
+            value: selectedDemo.wfreq,
+            title: 'Belly Button Washing Frequency',
+            type: 'indicator',
+            mode: 'gauge+number',
+            gauge: {
+                bar: {
+                    thickness: .5,
+                    color: 'darkslategray',
+                    line: {
+                        color: 'black'
+                    }
+                },
+                borderwidth: 1,
+                axis: {
+                    range: [0, 9],
+                    tickmode: 'array',
+                    ticks: '',
+                    tickvals: [1,2,3,4,5,6,7,8],
+                    ticktext: ['1','2','3','4','5','6','7','8']
+                },
+                steps: [
+                    {range: [0,1], color: 'maroon'},
+                    {range: [1,2], color: 'firebrick'},
+                    {range: [2,3], color: 'indianred'},
+                    {range: [3,4], color: 'burlywood'},
+                    {range: [4,5], color: 'khaki'},
+                    {range: [5,6], color: 'palegreen'},
+                    {range: [6,7], color: 'springgreen'},
+                    {range: [7,8], color: 'mediumseagreen'},
+                    {range: [8,9], color: 'seagreen'},
+                ]
+            }
+        };
+        
+        // Plots gauge chart for wfreq in the selected individual.
+        let gaugeData = [gaugeTrace];
+        let gaugeLayout = {
+            margin: { t:0, b:0 }
+        };
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
     }
+    updatePlotly();
 });
+
+
   
